28daabbaeee0faf39e0c4a4c62401774424a7056
Manuel Fittko <manuel.fittko@sofatutor.com>
2026-05-16 17:44:14 +0200

    docs: ignore generated presentation artifacts

--- docs/conductor-loop-latency-site/assets/chunk-7723GVCQ-BkoigA4e.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./chunk-PTVI3W5X-Bc8X9iyS.js";import{I as t}from"./chunk-V7P66DNM-C7ZvfPTd.js";var n=e((e,n)=>{let r;return n===`sandbox`&&(r=t(`#i`+e)),t(n===`sandbox`?r.nodes()[0].contentDocument.body:`body`).select(`[id="${e}"]`)},`getDiagramElement`);export{n as t};
